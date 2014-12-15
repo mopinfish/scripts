345383cb654b2af85e3d85a673b16030ef9cc57b
OtsukaNoboru <otsukano@OtsukaNoboru-no-MacBook-Pro.local>
2014-12-16 00:41:11 +0900

    [scripts] vagrant setup script for mac added.

---
+++ mac/vagrant_centos_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 1) Download and install Vagrant from url below.
+# http://www.vagrantup.com/downloads.html
+
+# 2) Download and install VirtualBox from url below.
+# https://www.virtualbox.org/wiki/Downloads
+
+# 3) add centos-box to vagrant.
+# vagrant box add centos64 http://developer.nrel.gov/downloads/vagrant-boxes/CentOS-6.4-x86_64-v20131103.box
+ 
+# 4) confirm box list.
+# vagrant box list
+
+# 5) make directory for vagrant.
+# mkdir -p ~/Vagrant/CentOS64
+# cd ~/Vagrant/CentOS64
+
+# 6) initialize vagrant of centos64 box.
+# vagrant init centos64
+
+# 7) starting vagrant.
+# vagrant up
+
+# 8) destroy vagrant runnning vm.
+# vagrant destroy
